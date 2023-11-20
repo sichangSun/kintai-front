14b33d7335c62141ce604c8e10614c0586f56468
kazuma-kanazawa <200033qb@yse-c.net>
2023-11-20 16:41:43 +0900

    add : ヘッダーメニュー画面

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HeaderMenu_screen/>
     <Login_screen/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import HeaderMenu_screen from './components/HeaderMenu_screen.vue';
 import Login_screen from './components/Login_screen.vue';
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Login_screen
+    Login_screen,
+    HeaderMenu_screen
 }
 }
 </script>
---
+++ src/components/HeaderMenu_screen.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="HeaderMenu">
+        <div class="Header">
+            <!-- <el-header> -->
+                <el-select v-model="value" clearable placeholder="メニュー">
+                    <el-option 
+                        v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                        :disabled="item.disabled">
+                    </el-option>
+                </el-select>
+            <!-- </el-header> -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    el: '#HeaderManu',
+    name: 'HeaderMenu_screen',
+    data() {
+        return {
+            options: [
+                { value: 'Option1', label: 'パスワード修正' },
+                { value: 'Option2', label: '社員管理', disabled: true },
+                { value: 'Option3', label: '新規社員追加', disabled: true },
+                { value: '', label: '' },
+                { value: '', label: '' },
+                { value: 'Option6', label: 'ログアウト' },
+            ],
+            value: '',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
