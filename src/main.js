ecb3f47e031bdf305f3d928a962ab76bceacb9ed
ヨウギョウヨウ <work.shawnyeung1995@gmail.com>
2023-11-28 17:46:39 +0900

    1.ElementUIの導入 2.新規「基本資料修正画面」 InfoUpdateForm.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+// ElementUI導入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/input.css'
 
 Vue.config.productionTip = false
 
+// ElementUI導入
+Vue.use(ElementUI);
+
 new Vue({
+  el:'#app',
   render: h => h(App),
 }).$mount('#app')
