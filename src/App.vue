dc2d691e5b2380d42020227b5657d6a50ef3b158
kazuma-kanazawa <200033qb@yse-c.net>
2023-11-29 19:02:12 +0900

    add : 社員画面

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,27 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <HeaderMenu_screen/>
-    <Administrator_screen/>
-    <Login_screen/>
+    <Employee_screen/>
+    <!-- <Administrator_screen/> -->
+    <!-- <Login_screen/> -->
 
-    <!-- <router-view></router-view> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Administrator_screen from './components/Administrator_screen.vue';
+// import Administrator_screen from './components/Administrator_screen.vue';
+import Employee_screen from './components/Employee_screen.vue';
 import HeaderMenu_screen from './components/HeaderMenu_screen.vue';
-import Login_screen from './components/Login_screen.vue';
+// import Login_screen from './components/Login_screen.vue';
 
 export default {
   name: 'App',
   components: {
-    Login_screen,
+    // Login_screen,
     HeaderMenu_screen,
-    Administrator_screen,
+    // Administrator_screen,
+    Employee_screen,
 }
 }
 </script>
